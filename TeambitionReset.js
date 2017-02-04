c2c6e5dd675da60dd8ef149293e100ccbd993759
xiaoping <edwardhjp@gmail.com>
2017-02-04 11:29:59 +0800

    change TeambitionStyleReset 2 TeambitionReset

---
+++ TeambitionReset.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Teambition Reset
+// @namespace    https://github.com/excaliburhan/LittleScripts
+// @version      0.2
+// @description  reset teambition
+// @author       xiaoping
+// @include      https://www.teambition.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  function showHiddenAct(e) {
+    let task = e.target.closest('.task')
+    let timer
+    if (!task) return
+    function clickHiddenBtn() {
+      timer = setTimeout(function() {
+        let hiddenBtn = document.querySelector('.activity-early-hidden')
+        if (hiddenBtn) {
+          hiddenBtn.click()
+          clearTimeout(timer)
+        } else {
+          clickHiddenBtn()
+        }
+      }, 200)
+    }
+    clickHiddenBtn()
+  }
+
+  // set pop action
+  document.body.addEventListener('click', showHiddenAct, false)
+
+  // set pop style
+  let css = '.object-modal-view.fixed { margin: 0; width: 70%; left: 15% }'
+  let head = document.head || document.getElementsByTagName('head')[0]
+  let style = document.createElement('style')
+
+  style.type = 'text/css'
+  style.appendChild(document.createTextNode(css))
+  head.appendChild(style)
+
+})()
